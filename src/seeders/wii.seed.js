82531793b0a14b3dcfd3f231fe255f8d4e2ca17e
Oscar Gomez <oscarthedev@gmail.com>
2022-07-31 12:44:08 -0600

    [PATCH] seeders listNum property patched

--- src/seeders/wii.seed.js
+++ src/seeders/wii.seed.js
@@ -6,7 +6,7 @@ mongoose.connect(MONGO_URI)
 const WiiGames = [
 	{
 		name: "Dragon Ball Z Budokai Tenkaichi 3",
-		listNum: 1,
+		listNum: 11,
 		console: "Wii",
 		description: "Dragon Ball Z: Budokai Tenkaichi 3, is the third installment of the Budokai Tenkaichi series. Like its predecessor, despite being released under the Dragon Ball Z label, Budokai Tenkaichi 3 essentially touches upon all series installments of the Dragon Ball franchise, featuring numerous characters and stages set in Dragon Ball, Dragon Ball Z, Dragon Ball GT and numerous film adaptations of Z.",
 		price: 20,
@@ -15,7 +15,7 @@ const WiiGames = [
 	},
 	{
 		name: "Wii Sports Resort",
-		listNum: 2,
+		listNum: 12,
 		console: "Wii",
 		description: "Wii Sports Resort is a new collection of fun sports games that anyone can pick up and play. This sequel to the popular Wii Sports makes use of the Wii MotionPlus accessory, which gives players the most responsive and realistic experience possible",
 		price: 20,
@@ -24,7 +24,7 @@ const WiiGames = [
 	},
 	{
 		name: "Super Mario Galaxy",
-		listNum: 3,
+		listNum: 13,
 		console: "Wii",
 		description: "Super Mario Galaxy is set in outer space, where Mario travels through different galaxies to collect Power Stars, earned by completing missions, defeating a boss, or reaching a particular area. Each galaxy contains planetoids and orbiting structures for the player to explore.",
 		price: 20,
@@ -33,7 +33,7 @@ const WiiGames = [
 	},
 	{
 		name: "Mario Sports Mix",
-		listNum: 4,
+		listNum: 14,
 		console: "Wii",
 		description: "Mario Sports Mix is a Wii video game by Nintendo released in 2010 in Japan. Mario Sports Mix features four playable sports, all unique from each other. Players can play either alone with CPU allies, cooperatively with others up to three players, or against one another.",
 		price: 20,
@@ -42,7 +42,7 @@ const WiiGames = [
 	},
 	{
 		name: "Mario Kart Wii",
-		listNum: 5,
+		listNum: 15,
 		console: "Wii",
 		description: "Mario Kart Wii is a 2008 kart racing video game developed and published by Nintendo for the Wii. It is the sixth installment in the Mario Kart series. Like its previous installments, Mario Kart Wii incorporates playable characters from the Mario series, who participate in races on over 30 different race tracks",
 		price: 20,
@@ -51,7 +51,7 @@ const WiiGames = [
 	},
 	{
 		name: "Donkey Kong Country Returns",
-		listNum: 6,
+		listNum: 16,
 		console: "Wii",
 		description: "Donkey Kong Country Returns is a side-scrolling platform game developed by Retro Studios and published by Nintendo for the Wii console. The game's story focuses on an evil group of Tiki-like creatures known as the Tiki Tak Tribe that are unleashed on Donkey Kong Island and hypnotize the island's animals to stealing Donkey Kong and Diddy Kong's banana hoard, prompting the two to traverse the island to reclaim it.",
 		price: 20,
@@ -60,7 +60,7 @@ const WiiGames = [
 	},
 	{
 		name: "Ben 10 Protector Of Earth",
-		listNum: 7,
+		listNum: 17,
 		console: "Wii",
 		description: "Ben 10 Protector of Earth' is a game where the player controls Ben and helps him to travel into 5 regions from the United States in order to recover all the Omnitrix's DNA samples stolen by Vilgax, which want use it to destroy the world.",
 		price: 20,
@@ -69,7 +69,7 @@ const WiiGames = [
 	},
 	{
 		name: "The Legend Of Zelda Twilight Princess",
-		listNum: 8,
+		listNum: 18,
 		console: "Wii",
 		description: "The Legend of Zelda: Twilight Princess[a] is a 2006 action-adventure game developed and published by Nintendo for the GameCube and Wii home video game consoles. The story focuses on series protagonist Link, who tries to prevent Hyrule from being engulfed by a corrupted parallel dimension known as the Twilight Realm. To do so, he takes the form of both a Hylian and a wolf, and he is assisted by a mysterious creature named Midna.",
 		price: 20,
@@ -78,7 +78,7 @@ const WiiGames = [
 	},
 	{
 		name: "New Super Mario Bros Wii",
-		listNum: 9,
+		listNum: 19,
 		console: "Wii",
 		description: "New Super Mario Bros. Wii is a 2D side-scrolling platform video game developed by Nintendo for the Wii. New Super Mario Bros. Wii is the first Super Mario game to feature simultaneous cooperative multiplayer gameplay. Up to four players, as either Mario, his brother Luigi, or one of two different colored Toads, can play through levels together.",
 		price: 20,
@@ -87,7 +87,7 @@ const WiiGames = [
 	},
 	{
 		name: "Lego Batman",
-		listNum: 10,
+		listNum: 20,
 		console: "Wii",
 		description: "The core gameplay of Lego Batman is similar to that of previous Lego video games, such as Lego Indiana Jones: The Original Adventures and the Lego Star Wars series. ... The game is set in Gotham City, with mainly realistic environments; only interactive objects are made of Lego bricks.",
 		price: 20,
--- src/seeders/xbox.seed.js
+++ src/seeders/xbox.seed.js
@@ -6,7 +6,7 @@ mongoose.connect(MONGO_URI)
 const XboxGames = [
 	{
 		name: "Call of Duty Black Ops 2",
-		listNum: 1,
+		listNum: 21,
 		console: "Xbox360",
 		description: "Pushing the boundaries of what fans have come to expect from the record-setting entertainment franchise, Call of Duty®: Black Ops II propels players into a near future, 21st Century Cold War, where technology and weapons have converged to create a new generation of warfare.",
 		price: 20,
@@ -15,7 +15,7 @@ const XboxGames = [
 	},
 	{
 		name: "Minecraft",
-		listNum: 2,
+		listNum: 22,
 		console: "Xbox360",
 		description: "Minecraft is a video game in which players create and break apart various kinds of blocks in three-dimensional worlds. The game's two main modes are Survival and Creative. In Survival, players must find their own building supplies and food. ... In Creative, players are given supplies and do not have to eat to survive.",
 		price: 20,
@@ -24,7 +24,7 @@ const XboxGames = [
 	},
 	{
 		name: "Grand theft Auto IV",
-		listNum: 3,
+		listNum: 23,
 		console: "Xbox360",
 		description: `In Grand Theft Auto IV you take on the role of Niko Bellic, an immigrant from Eastern Europe with an ominous past. Persuaded by his cousin's promises of a fresh start and a better life, Niko relocates to Liberty City, USA -- the worst place in America. After making the move to Liberty, however, Niko quickly realizes that his hopes of "living the American Dream" may never come to pass... and that things here may not be so different after all.`,
 		price: 20,
@@ -33,7 +33,7 @@ const XboxGames = [
 	},
 	{
 		name: "Grand Theft Auto V",
-		listNum: 4,
+		listNum: 24,
 		console: "Xbox360",
 		description: "Grand Theft Auto V is an action-adventure game played from either a third-person or first-person perspective. Players complete missions—linear scenarios with set objectives—to progress through the story. Outside of the missions, players may freely roam the open world.",
 		price: 20,
@@ -42,7 +42,7 @@ const XboxGames = [
 	},
 	{
 		name: "Guitar Hero 3",
-		listNum: 5,
+		listNum: 25,
 		console: "Xbox360",
 		description: "Guitar Hero III: Legends of Rock is a music rhythm video game developed by Neversoft and published by Activision. ... Initially the game offers over 70 songs, most of which are master tracks. The PlayStation 3 and Xbox 360 versions feature the ability to download additional songs.",
 		price: 20,
@@ -51,7 +51,7 @@ const XboxGames = [
 	},
 	{
 		name: "Mortal Kombat 9",
-		listNum: 6,
+		listNum: 26,
 		console: "Xbox360",
 		description: "Mortal Kombat is a fighting video game developed by NetherRealm Studios and published by Warner Bros. The game is the ninth main installment in the Mortal Kombat series and a soft reboot of the franchise.",
 		price: 20,
@@ -60,7 +60,7 @@ const XboxGames = [
 	},
 	{
 		name: "Far Cry 3",
-		listNum: 7,
+		listNum: 27,
 		console: "Xbox360",
 		description: "Far Cry 3 is a first-person shooter set on the fictional Rook Islands, a tropical archipelago somewhere in the Pacific, controlled by pirates and mercenaries. Players portray Jason Brody and can approach missions and objectives in a variety of ways.",
 		price: 20,
@@ -69,7 +69,7 @@ const XboxGames = [
 	},
 	{
 		name: "Left 4 Dead 2",
-		listNum: 8,
+		listNum: 28,
 		console: "Xbox360",
 		description: "Left 4 Dead 2 is a 2009 first-person shooter game developed and published by Valve. ... Set during the aftermath of an apocalyptic pandemic, the game focuses on four new Survivors, fighting against hordes of zombies known as the Infected, who develop severe psychosis and act extremely aggressive.",
 		price: 20,
@@ -78,7 +78,7 @@ const XboxGames = [
 	},
 	{
 		name: "Wipeout",
-		listNum: 9,
+		listNum: 29,
 		console: "Xbox360",
 		description: "This is a game based on the TV game show Wipeout. Players create and compete in obstacle courses that include hazards such as swinging pads, punching gloves, and giant wrecking balls.",
 		price: 20,
@@ -87,7 +87,7 @@ const XboxGames = [
 	},
 	{
 		name: "Tomb Raider",
-		listNum: 10,
+		listNum: 30,
 		console: "Xbox360",
 		description: "Tomb Raider is set on Yamatai, an island from which Lara, who is untested and not yet the battle-hardened explorer she is in other titles in the series, must save her friends and escape while being hunted down by a malevolent cult.",
 		price: 20,
