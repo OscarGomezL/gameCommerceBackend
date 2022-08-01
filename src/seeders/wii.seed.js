d2a1f2f02e8304f20444408a441175a206034a14
Oscar Gomez <oscarthedev@gmail.com>
2022-08-01 12:54:31 -0600

    [PATCH] wii seed logo name patched

--- src/seeders/wii.seed.js
+++ src/seeders/wii.seed.js
@@ -11,7 +11,7 @@ const WiiGames = [
 		description: "Dragon Ball Z: Budokai Tenkaichi 3, is the third installment of the Budokai Tenkaichi series. Like its predecessor, despite being released under the Dragon Ball Z label, Budokai Tenkaichi 3 essentially touches upon all series installments of the Dragon Ball franchise, featuring numerous characters and stages set in Dragon Ball, Dragon Ball Z, Dragon Ball GT and numerous film adaptations of Z.",
 		price: 20,
 		quantity: 1,
-		logo: "wii1.jpg",
+		logo: "wii11.jpg",
 	},
 	{
 		name: "Wii Sports Resort",
@@ -20,7 +20,7 @@ const WiiGames = [
 		description: "Wii Sports Resort is a new collection of fun sports games that anyone can pick up and play. This sequel to the popular Wii Sports makes use of the Wii MotionPlus accessory, which gives players the most responsive and realistic experience possible",
 		price: 20,
 		quantity: 1,
-		logo: "wii2.jpg",
+		logo: "wii12.jpg",
 	},
 	{
 		name: "Super Mario Galaxy",
@@ -29,7 +29,7 @@ const WiiGames = [
 		description: "Super Mario Galaxy is set in outer space, where Mario travels through different galaxies to collect Power Stars, earned by completing missions, defeating a boss, or reaching a particular area. Each galaxy contains planetoids and orbiting structures for the player to explore.",
 		price: 20,
 		quantity: 1,
-		logo: "wii3.jpg",
+		logo: "wii13.jpg",
 	},
 	{
 		name: "Mario Sports Mix",
@@ -38,7 +38,7 @@ const WiiGames = [
 		description: "Mario Sports Mix is a Wii video game by Nintendo released in 2010 in Japan. Mario Sports Mix features four playable sports, all unique from each other. Players can play either alone with CPU allies, cooperatively with others up to three players, or against one another.",
 		price: 20,
 		quantity: 1,
-		logo: "wii4.jpg",
+		logo: "wii14.jpg",
 	},
 	{
 		name: "Mario Kart Wii",
@@ -47,7 +47,7 @@ const WiiGames = [
 		description: "Mario Kart Wii is a 2008 kart racing video game developed and published by Nintendo for the Wii. It is the sixth installment in the Mario Kart series. Like its previous installments, Mario Kart Wii incorporates playable characters from the Mario series, who participate in races on over 30 different race tracks",
 		price: 20,
 		quantity: 1,
-		logo: "wii5.jpg",
+		logo: "wii15.jpg",
 	},
 	{
 		name: "Donkey Kong Country Returns",
@@ -56,7 +56,7 @@ const WiiGames = [
 		description: "Donkey Kong Country Returns is a side-scrolling platform game developed by Retro Studios and published by Nintendo for the Wii console. The game's story focuses on an evil group of Tiki-like creatures known as the Tiki Tak Tribe that are unleashed on Donkey Kong Island and hypnotize the island's animals to stealing Donkey Kong and Diddy Kong's banana hoard, prompting the two to traverse the island to reclaim it.",
 		price: 20,
 		quantity: 1,
-		logo: "wii6.jpg",
+		logo: "wii16.jpg",
 	},
 	{
 		name: "Ben 10 Protector Of Earth",
@@ -65,7 +65,7 @@ const WiiGames = [
 		description: "Ben 10 Protector of Earth' is a game where the player controls Ben and helps him to travel into 5 regions from the United States in order to recover all the Omnitrix's DNA samples stolen by Vilgax, which want use it to destroy the world.",
 		price: 20,
 		quantity: 1,
-		logo: "wii7.jpg",
+		logo: "wii17.jpg",
 	},
 	{
 		name: "The Legend Of Zelda Twilight Princess",
@@ -74,7 +74,7 @@ const WiiGames = [
 		description: "The Legend of Zelda: Twilight Princess[a] is a 2006 action-adventure game developed and published by Nintendo for the GameCube and Wii home video game consoles. The story focuses on series protagonist Link, who tries to prevent Hyrule from being engulfed by a corrupted parallel dimension known as the Twilight Realm. To do so, he takes the form of both a Hylian and a wolf, and he is assisted by a mysterious creature named Midna.",
 		price: 20,
 		quantity: 1,
-		logo: "wii8.jpg",
+		logo: "wii18.jpg",
 	},
 	{
 		name: "New Super Mario Bros Wii",
@@ -83,7 +83,7 @@ const WiiGames = [
 		description: "New Super Mario Bros. Wii is a 2D side-scrolling platform video game developed by Nintendo for the Wii. New Super Mario Bros. Wii is the first Super Mario game to feature simultaneous cooperative multiplayer gameplay. Up to four players, as either Mario, his brother Luigi, or one of two different colored Toads, can play through levels together.",
 		price: 20,
 		quantity: 1,
-		logo: "wii9.jpg",
+		logo: "wii19.jpg",
 	},
 	{
 		name: "Lego Batman",
@@ -92,7 +92,7 @@ const WiiGames = [
 		description: "The core gameplay of Lego Batman is similar to that of previous Lego video games, such as Lego Indiana Jones: The Original Adventures and the Lego Star Wars series. ... The game is set in Gotham City, with mainly realistic environments; only interactive objects are made of Lego bricks.",
 		price: 20,
 		quantity: 1,
-		logo: "wii10.jpg",
+		logo: "wii20.jpg",
 	}
 ]
 GameModel.create(WiiGames).then(()=>{
